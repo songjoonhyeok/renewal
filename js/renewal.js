689554c9628526b78aa789de294228ad9c79de21
PC3\Administrator <dnvkds@naver.com>
2017-08-22 16:56:47 +0900

    다시푸쉬

---
+++ js/renewal.js
@@ -0,0 +1,28 @@
+$(function(){
+
+
+  // $(window).scroll(function(){
+  //    var src = $(this).scrollTop();
+  //
+  //    if (src >= 70){
+  //
+  //      $('.nav').css({
+  //        'marginTop':0
+  //      })
+  //    }else if (src <= 70){
+  //      $('.nav').css({
+  //        'marginTop':'70px'
+  //      })
+  //    }
+  // })
+
+
+
+
+
+
+
+
+
+
+})
